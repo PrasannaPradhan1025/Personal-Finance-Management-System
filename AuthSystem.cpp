8f561c82050d326e3e61ec334a4de58747c6be94
PrasannaPradhan1025 <prasanna.pradhan179@gmail.com>
2026-08-08 12:27:51 +0545

    Authentication for SignUp

--- AuthSystem.cpp
+++ AuthSystem.cpp
@@ -1,14 +1,58 @@
 #include "AuthSystem.h"
+#include <iostream>
+#include <cctype>
+#include <string>
 
 AuthSystem::AuthSystem(UserManager &manager)
-    : userManager(manager)
+    : userManager(manager), currentUser(nullptr)
 {
 }
+
+bool AuthSystem::isStrongPassword(const std::string& password){
+    bool hasNumber = false;
+    bool hasUppercase = false;
+
+    for (char c : password) {
+        if (std::isdigit(static_cast<unsigned char>(c))) {
+            hasNumber = true;
+        }
+
+        if (std::isupper(static_cast<unsigned char>(c))) {
+            hasUppercase = true;
+        }
+    }
+
+    return hasNumber && hasUppercase;
+}
+
 bool AuthSystem::signup(const std::string &username,
                         const std::string &password)
 {
-    if (userManager.usernameExists(username))
+
+
+
+    if (userManager.usernameExists(username) )
     {
+        return false;
+    }
+
+    if (username.length() > 20 || password.length() > 20) {
+        std::cout << "Username and password should not exceed 20 characters.\n";
+        return false;
+    }
+
+    if (username.length() < 3 || password.length() < 6) {
+        std::cout << "Username must be at least 3 characters long and password must be at least 6 characters long.\n";
+        return false;
+    }
+
+    if (username.empty() || password.empty() ) {
+        std::cout << "Username and password cannot be empty.\n";
+        return false;   
+    }
+
+    if(isStrongPassword(password) == false){
+        std::cout << "Password must contain at least one number and one uppercase letter.\n";
         return false;
     }
 
@@ -16,3 +60,5 @@ bool AuthSystem::signup(const std::string &username,
 
     return true;
 }
+
+
--- AuthSystem.h
+++ AuthSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "UserManager.h"
+
 class AuthSystem
 {
 private:
@@ -10,4 +11,7 @@ public:
     AuthSystem(UserManager &manager);
     bool signup(const std::string &username,
                 const std::string &password);
+
+    bool isStrongPassword(const std::string& password);
+   
 };
