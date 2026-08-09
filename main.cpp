3a2a3677d15a8d47cec269bd84d7292623da13f0
samipjsh <samipjsh2064@gmail.com>
2026-08-09 13:40:07 +0545

    updated main
    updated main to fit everything in the menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "User.h"
 #include "UserManager.h"
 #include "AuthSystem.h"
+#include "IncomeManager.h"
+#include "ExpenseManager.h"
+#include "TransactionManager.h"
 #include "utils.h"
 
 
@@ -46,36 +49,176 @@ int main()
             {
                 cout << "Login successful!\n";
                 cout << "Welcome, " << auth.getCurrentUser()->getUsername() << "!\n";
+
+                IncomeManager incomeManager("Income.txt", auth.getCurrentUser()->getId());
+                ExpenseManager expenseManager("Expenses.txt", auth.getCurrentUser()->getId());
+                TransactionManager transactionManager(incomeManager.getIncomeForCurrentUser(),
+                                                      expenseManager.getExpensesForCurrentUser(),
+                                                      auth.getCurrentUser()->getId());
+
                 while (true)
                 {
-                    cout << "\n==== Personal Finance Menu ====\n";
-                    cout << "1. View Profile\n";
-                    // Additional personal finance features can be added here
+                    cout << "\n==== FINANCE MANAGER ====\n";
+                    cout << "1. Income\n";
+                    cout << "2. Expenses\n";
+                    cout << "3. Transactions\n";
+                    cout << "4. View Profile\n";
                     cout << "5. Logout\n";
                     int financeChoice;
                     cin >> financeChoice;
                     switch (financeChoice)
                     {
                     case 1:
+                        while (true)
+                        {
+                            cout << "\n==== INCOME ====\n";
+                            cout << "1. Add Income\n";
+                            cout << "2. View Income\n";
+                            cout << "3. Update Income\n";
+                            cout << "4. Delete Income\n";
+                            cout << "5. Search Income\n";
+                            cout << "6. Back\n";
+                            int incomeChoice;
+                            cin >> incomeChoice;
+                            switch (incomeChoice)
+                            {
+                            case 1:
+                                incomeManager.addIncome();
+                                break;
+                            case 2:
+                                incomeManager.viewIncome();
+                                break;
+                            case 3:
+                                incomeManager.updateIncome();
+                                break;
+                            case 4:
+                                incomeManager.deleteIncome();
+                                break;
+                            case 5:
+                                incomeManager.searchIncome();
+                                break;
+                            case 6:
+                                break;
+                            default:
+                                cout << "Invalid choice!\n";
+                            }
+                            if (incomeChoice == 6)
+                            {
+                                break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        while (true)
+                        {
+                            cout << "\n==== EXPENSES ====\n";
+                            cout << "1. Add Expense\n";
+                            cout << "2. View Expenses\n";
+                            cout << "3. Update Expense\n";
+                            cout << "4. Delete Expense\n";
+                            cout << "5. Search Expense\n";
+                            cout << "6. Back\n";
+                            int expenseChoice;
+                            cin >> expenseChoice;
+                            switch (expenseChoice)
+                            {
+                            case 1:
+                                expenseManager.addExpense();
+                                break;
+                            case 2:
+                                expenseManager.viewExpenses();
+                                break;
+                            case 3:
+                                expenseManager.updateExpense();
+                                break;
+                            case 4:
+                                expenseManager.deleteExpense();
+                                break;
+                            case 5:
+                                expenseManager.searchExpense();
+                                break;
+                            case 6:
+                                break;
+                            default:
+                                cout << "Invalid choice!\n";
+                            }
+                            if (expenseChoice == 6)
+                            {
+                                break;
+                            }
+                        }
+                        break;
+                    case 3:
+                        while (true)
+                        {
+                            TransactionManager transactionManager(incomeManager.getIncomeForCurrentUser(),
+                                                                  expenseManager.getExpensesForCurrentUser(),
+                                                                  auth.getCurrentUser()->getId());
+                            cout << "\n==== TRANSACTIONS ====\n";
+                            cout << "1. View All Transactions\n";
+                            cout << "2. Search\n";
+                            cout << "3. Filter by Type\n";
+                            cout << "4. Filter by Date\n";
+                            cout << "5. Filter by Category\n";
+                            cout << "6. Sort by Amount\n";
+                            cout << "7. Sort by Date\n";
+                            cout << "8. Back\n";
+                            int transactionChoice;
+                            cin >> transactionChoice;
+                            switch (transactionChoice)
+                            {
+                            case 1:
+                                transactionManager.viewTransactions();
+                                break;
+                            case 2:
+                                cin.ignore();
+                                transactionManager.searchTransaction();
+                                break;
+                            case 3:
+                                transactionManager.filterByType();
+                                break;
+                            case 4:
+                                cin.ignore();
+                                transactionManager.filterByDate();
+                                break;
+                            case 5:
+                                cin.ignore();
+                                transactionManager.filterByCategory();
+                                break;
+                            case 6:
+                                transactionManager.sortByAmount();
+                                break;
+                            case 7:
+                                transactionManager.sortByDate();
+                                break;
+                            case 8:
+                                break;
+                            default:
+                                cout << "Invalid choice!\n";
+                            }
+                            if (transactionChoice == 8)
+                            {
+                                break;
+                            }
+                        }
+                        break;
+                    case 4:
                         cout << "User Profile:\n";
                         cout << "Username: " << auth.getCurrentUser()->getUsername() << endl;
                         cout << "User ID: " << auth.getCurrentUser()->getId() << endl;
-                        // additionals
                         break;
                     case 5:
                         auth.logout();
                         cout << "Logged out successfully!\n";
-
                         break;
                     default:
                         cout << "Invalid choice!\n";
                     }
                     if (financeChoice == 5)
                     {
-                        break; // exit the personal finance menu loop
+                        break;
                     }
                 }
-                // personal finance menu and further features can be implemented here
             }
             else
             {
